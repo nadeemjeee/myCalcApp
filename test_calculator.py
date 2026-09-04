419389f2ed3cd46765e43c8f09f9bd30b4a82b1c
Nadeem Ahmad <nadeemjeee@gmail.com>
2026-09-04 16:44:09 +0200

    added power branch with its test

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide, power
 
 def test_add():
     assert add(2, 3) == 5
@@ -18,3 +18,5 @@ def test_divide_by_zero():
         assert False, "Expected an error but didn't get one"
     except ValueError:
         assert True
+def test_power():
+    assert power(2, 3) == 8
